ab0aabf06d84dba24c958eb30160bca13eb39a2f
JonasHeim <heimjonas@yahoo.de>
2019-03-17 13:48:54 +0100

    Initial commit.

---
+++ Civ5 Savegame Editor/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Erstellt mit SharpDevelop.
+ * Benutzer: ForceXX
+ * Datum: 17.03.2019
+ * Zeit: 10:36
+ * 
+ * Sie können diese Vorlage unter Extras > Optionen > Codeerstellung > Standardheader ändern.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace Civ5_Savegame_Editor
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
